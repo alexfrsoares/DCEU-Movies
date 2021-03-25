0be5824394ad3afcc10f725e1051c6701af6f9e2
Alex <otimizalex@outlook.com>
2021-03-25 16:00:19 -0300

    Initial Commit

---
+++ DC Movies/DC_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DC_MoviesApp.swift
+//  DC Movies
+//
+//  Created by Alex Freitas on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DC_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
